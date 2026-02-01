397ec50229b2529cb78379ee54a80c486813302a
Savas Gioldasis <savvas.gioldasis@ing.com>
2026-02-01 12:38:17 +0200

    Add ngrok

--- dbt/modern-dashboard/frontend/vite.config.js
+++ dbt/modern-dashboard/frontend/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 3000,
+        allowedHosts: true,
         proxy: {
             '/api': 'http://localhost:8000'
         }
